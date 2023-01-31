1264f2e6b19e4297bd4b08b9108f196bb60afddb
Daniel Alejandro Gaona Mercado <daniel.gaona@pjedomex.gob.mx>
2023-01-30 22:26:29 -0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import Button from "./components/button/Button";
+
+import { useState,useRef,useEffect } from 'react';
+import * as MediaLibrary from 'expo-media-library';
+import { Camera, CameraType} from 'expo-camera';
+
+import CardImagen from './components/card/CardImagen';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permisos, setPermisos] = Camera.useCameraPermissions();
+  const [image, setImage] = useState(null);
+  const cameraReferencia = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setPermisos(cameraStatus.status === 'granted');
+    })();
+  }, []);
+  
+  const tomarFoto = async()=>{
+    if (cameraReferencia){
+      try {
+        const data = await cameraReferencia.current.takePictureAsync();
+        setImage(data.uri);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  if (!permisos) {
+    return <View />;
+  }
+
+  if (!permisos.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>Necesitas permisos para acceder a la camara </Text>
+        <Button label={"Permisos"} onHandlerPress={setPermisos} size={20} color={"#FF00FF"} styles={styles} />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Camera style={styles.camera} type={type} flashMwode={0} ref={cameraReferencia}>
+        <View style={styles.buttonContainer}>
+          <CardImagen url={""} diseño={styles} imagen={image} />
+          
+        </View>
+      </Camera>
+      <View style={styles.buttonContainer}>
+        <Button label={"Tomar Foto"} onHandlerPress={tomarFoto} icon={"camera"} size={20} color={"#FF00FF"} styles={styles} />
+        <Button label={"Cambiar camara"} onHandlerPress={()=>setType(type === CameraType.back ? CameraType.front : CameraType.back)} icon={"refresh"} size={20} color={"#FF00FF"} styles={styles} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +69,41 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    padding: 10,
+    margin: 10,
+  },
+  buttonIcon: {
+    paddingRight: 10,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    color: "#25292e"
+  },
+  camera: {
+    flex: 1,
+    width: '80%',
+    height: '100%',
+  }, 
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 5,
+  },
+  image: {
+    width: '100px',
+    height: '100px',
+    borderRadius: 5,
+    borderStyle: '1px solid #FFF'
   },
 });
