a005f250b6ff450f64fac6ab4f2815b5a08c355a
Daniel Alejandro Gaona Mercado <daniel.gaona@pjedomex.gob.mx>
2023-01-31 17:51:22 -0600

    save imagen cam

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
 import Button from "./components/button/Button";
 
-import { useState,useRef,useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import * as MediaLibrary from 'expo-media-library';
-import { Camera, CameraType} from 'expo-camera';
+import { Camera, CameraType } from 'expo-camera';
 
 import CardImagen from './components/card/CardImagen';
 
@@ -21,12 +21,21 @@ export default function App() {
       setPermisos(cameraStatus.status === 'granted');
     })();
   }, []);
-  
-  const tomarFoto = async()=>{
-    if (cameraReferencia){
+
+  const tomarFoto = async () => {
+    if (cameraReferencia) {
       try {
         const data = await cameraReferencia.current.takePictureAsync();
         setImage(data.uri);
+
+        const asset = await MediaLibrary.createAssetAsync(data.uri);
+        MediaLibrary.createAlbumAsync('Expo', asset)
+          .then(() => {
+            console.log('Album created!');
+          })
+          .catch(error => {
+            console.log('err', error);
+          });
       } catch (error) {
         console.log(error);
       }
@@ -51,12 +60,12 @@ export default function App() {
       <Camera style={styles.camera} type={type} flashMwode={0} ref={cameraReferencia}>
         <View style={styles.buttonContainer}>
           <CardImagen url={""} diseño={styles} imagen={image} />
-          
+
         </View>
       </Camera>
       <View style={styles.buttonContainer}>
         <Button label={"Tomar Foto"} onHandlerPress={tomarFoto} icon={"camera"} size={20} color={"#FF00FF"} styles={styles} />
-        <Button label={"Cambiar camara"} onHandlerPress={()=>setType(type === CameraType.back ? CameraType.front : CameraType.back)} icon={"refresh"} size={20} color={"#FF00FF"} styles={styles} />
+        <Button label={"Cambiar camara"} onHandlerPress={() => setType(type === CameraType.back ? CameraType.front : CameraType.back)} icon={"refresh"} size={20} color={"#FF00FF"} styles={styles} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -89,7 +98,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '80%',
     height: '100%',
-  }, 
+  },
   buttonContainer: {
     flex: 1,
     flexDirection: 'row',
